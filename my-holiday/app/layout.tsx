52f4517c4103a3d8cfc46134e369fbd9ec366974
Gürkan <gurkanyigitgr@gmail.com>
2023-07-29 03:42:07 +0300

    add MountedClient for childs

--- my-holiday/app/layout.tsx
+++ my-holiday/app/layout.tsx
@@ -1,11 +1,28 @@
 import "../styles/globals.css";
+import { Figtree } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import MountedClient from "./components/MountedClient";
+import RegisterModal from "./components/modals/RegisterModal";
+
+const newFont = Figtree({
+  subsets: ["latin"],
+});
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
+      <body className={newFont.className}>
+        <MountedClient>
+          {/* <Modal
+            isOpen
+            onSubmit={() => {}}
+            onClose={() => {}}
+            btnLabel="Register"
+            title="Register"
+          /> */}
+          <RegisterModal />
+          <Navbar />
+        </MountedClient>
         {children}
       </body>
     </html>
