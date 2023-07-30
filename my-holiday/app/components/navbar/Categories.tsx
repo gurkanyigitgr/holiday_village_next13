86cf892bab54bbc4e63ef0fd6416e029658e758c
Gürkan <gurkanyigitgr@gmail.com>
2023-07-30 15:02:12 +0300

    add categories

---
+++ my-holiday/app/components/navbar/Categories.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { FaUmbrellaBeach } from "react-icons/fa";
+import { LuHotel } from "react-icons/lu";
+import CategoriesItem from "./CategoriesItem";
+import { useSearchParams } from "next/navigation";
+
+export const categories = [
+  {
+    name: "Tatil-Köyü",
+    icon: FaUmbrellaBeach,
+  },
+  {
+    name: "Kamp",
+    icon: LuHotel,
+  },
+  {
+    name: "Otel",
+    icon: FaUmbrellaBeach,
+  },
+  {
+    name: "Villa",
+    icon: LuHotel,
+  },
+];
+
+const Categories = () => {
+  const params = useSearchParams();
+  const urlItem = params?.get("category");
+
+  return (
+    <div className="flex items-center space-x-28">
+      {categories.map((cat, i) => (
+        <CategoriesItem
+          key={i}
+          name={cat.name}
+          icon={cat.icon}
+          selected={urlItem == cat.name ? true : false}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- my-holiday/app/components/navbar/Navbar.tsx
+++ my-holiday/app/components/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import Category from "./Category";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between h-16 bg-gray-100">
+    <div className="flex items-center justify-between h-16 bg-gray-100 px-7">
       <Logo />
-      <Category />
+      <Categories />
       <UserMenu />
     </div>
   );
