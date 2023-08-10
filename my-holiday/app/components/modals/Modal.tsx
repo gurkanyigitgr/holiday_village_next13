982725e913e8562afec8ced9408e4694f5779e3c
Gürkan <gurkanyigitgr@gmail.com>
2023-08-11 01:52:20 +0300

    add handleOutsideClick for modals

--- my-holiday/app/components/modals/Modal.tsx
+++ my-holiday/app/components/modals/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
-
 import { GrClose } from "react-icons/gr";
 import Button from "../buttons/Button";
+import { useEffect, useRef } from "react";
 
 type ModalProps = {
   isOpen: boolean;
@@ -21,18 +21,39 @@ const Modal: React.FC<ModalProps> = ({
   bodyElement,
   footerElement,
 }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
   const closeFunc = () => {
     onClose();
   };
   const submitFunc = () => {
     onSubmit();
   };
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
   return (
     <div className="bg-black bg-opacity-70 fixed flex items-center justify-center w-full h-full">
-      <div className="bg-white rounded-lg p-5 w-1/2">
+      <div className="bg-white rounded-lg p-5 w-1/2" ref={modalRef}>
         <div className="flex items-center justify-between pb-3 mb-3 border-b">
           <div className="text-2xl ">{title}</div>
           <div onClick={closeFunc}>
